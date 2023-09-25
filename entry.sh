9c9218e58fe31990a1098455678270c4c8d2efe4
Harrison Gulliver <harrison@thenewthirty.co.nz>
2023-09-25 13:05:14 +1300

    Fix environment variables

--- entry.sh
+++ entry.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+: "${PORT:=8080}"
+: "${DOCUMENT_ROOT:=/usr/share/nginx/html}"
+
+envsubst '${PORT},${DOCUMENT_ROOT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 /usr/sbin/php-fpm -D && nginx -g "daemon off;"
